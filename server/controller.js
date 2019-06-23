6b7a7bc296b1d798a069f1ec85df20c8283a0c85
Angela Choi <angelachoi@Angelas-MacBook-Air.local>
2019-06-23 16:26:24 -0700

    added createuser function

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,23 @@
-const db = require('../db/index.js')
+const db = require('../db/index.js');
 
 module.exports = {
+
+  getUser: (req, res) => {
+    console.log('ar you tryna login')
+  },
+  createUser: (req, res) => {
+    const { email, passphrase, gender, age } = req.body;
+    const first_name = req.body.first_name[0].toUpperCase() + req.body.first_name.slice(1).toLowerCase();
+    const last_name = req.body.last_name[0].toUpperCase() + req.body.last_name.slice(1).toLowerCase();
+    //grabs all fields required to sign up for an account. Proper capitalization for first name and last name. Inserts into users table.
+    //current database does not take into count of unique emails.
+    db.query(`INSERT INTO users(email, passphrase, first_name, last_name, gender, age) VALUES('${email}', '${passphrase}', '${first_name}', '${last_name}', '${gender}', ${age}) RETURNING *;`)
+      .then(data =>  res.status(200).send(data.rows[0]))
+      .catch(e => res.status(404).send(e.stack))
+  },
+  editUser: (req, res) => {
+    console.log('are u trying to lie about urself')
+  },
   getOne: (req, res) => {
 
   },
--- server/router.js
+++ server/router.js
@@ -4,15 +4,16 @@ const controller = require('./controller.js')
 //USERS: create account/login/edit account
 router
   .route('/user')
-  .get(controller.getAll)
-  .post(controller.post)
+    .get(controller.getUser) // allows user to log in with id/email and salted pw
+    .post(controller.createUser) // allows user to create an account
+    .put(controller.editUser) // allows user to edit account info
 
 //POSTS: create post GET/edit post PATCH/see all posts GET/ see one post GET
 router
   .route('/post')
-  .delete(controller.deleteOne)
-  .get(controller.getOne)
-  .patch(controller.patch)
+  // .delete(controller.deleteOne)
+  // .get(controller.getOne)
+  // .patch(controller.patch)
 
 //ATTENDEES: post to attendees/ get attendees/ (confirm/deny) patch boolean
 router
