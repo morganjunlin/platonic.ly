d5e05bea26a79c28545d497467c1dabcb6b4d084
Angela Choi <eunsol.angela.choi@gmail.com>
2019-06-23 17:21:49 -0700

    finished fleshing out all functions.

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,7 @@ const db = require('../db/index.js');
 
 module.exports = {
 
-  getUser: (req, res) => {
+  getUser: (req, res) => { // logging into an account
     const { email, password } = req.body;
     const passphrase = password; // ready for salting. apply hashing function to password.
     // a user inputs email and password to log in to their account. If the credentials are correct, it will send back a success message. If incorrect credentials, it will send a defense message.
@@ -11,7 +11,7 @@ module.exports = {
       .then(data => data.rows.length ? res.status(200).send(`correct credentials! data.rows[0] has your login info`) : res.status(200).send(`are you a hacker`) )
       .catch(e => res.status(404).send(e.stack))
   },
-  createUser: (req, res) => {
+  createUser: (req, res) => { // signing up for an account
     const { email, password, gender, age } = req.body;
     const passphrase = password; // ready for salting. apply hashing function to password.
     const first_name = req.body.first_name[0].toUpperCase() + req.body.first_name.slice(1).toLowerCase();
@@ -23,8 +23,18 @@ module.exports = {
       .then(data =>  res.status(200).send(data.rows[0]))
       .catch(e => res.status(404).send(e.stack))
   },
-  editUser: (req, res) => {
-    console.log('are u trying to lie about urself')
+  editUser: (req, res) => { // editing info of an account based on their email.
+    const { email, password, gender, age } = req.body;
+    const passphrase = password; // ready for salting. apply hashing function to password.
+    const first_name = req.body.first_name[0].toUpperCase() + req.body.first_name.slice(1).toLowerCase();
+    const last_name = req.body.last_name[0].toUpperCase() + req.body.last_name.slice(1).toLowerCase();
+    //grabs all fields of users row and edits all of them, even if there aren't edits made.
+    // For example, a user might want to edit only their gender. This function edits every column of that user's row (found by email).
+    // an edit request will require email, password, gender, age, first_name, last_name fields.
+    // this function can be updated in the future based on our application's needs.
+    db.query(`UPDATE users SET passphrase = '${passphrase}', first_name = '${first_name}', last_name = '${last_name}', gender = '${gender}', age = ${age} WHERE email = '${email}'`)
+      .then(data =>  res.status(200).send(`Updated info for ${email}!`))
+      .catch(e => res.status(404).send(e.stack))
   },
   getOne: (req, res) => {
   },
