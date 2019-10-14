ef6d208238fa67d52c447357300ddf8506d96e5c
morganl92 <morganlinbox@gmail.com>
2019-10-14 16:47:30 -0700

    Code cleanup

--- client/navigation/MainTabNavigator.js
+++ client/navigation/MainTabNavigator.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-// import LinksScreen from '../screens/LinksScreen';
-// import SettingsScreen from '../screens/SettingsScreen';
 
+import AllPost from '../screens/homeComponents/AllPosts';
 import HomeScreen from '../screens/homeComponents/HomeScreen';
-import IndividualPost from '../screens/homeComponents/IndividualPost';
 import HostPost from '../screens/homeComponents/HostPost';
-
+import IndividualPost from '../screens/homeComponents/IndividualPost';
 
 import PostConfirmScreen from '../screens/post/postConfirm';
 import PostScreen from '../screens/post/postScreen';
+
 import ProfileScreen from '../screens/ProfileScreen';
-import AllPost from '../screens/homeComponents/AllPosts';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -40,20 +35,6 @@ HomeStack.navigationOptions = {
   ),
 };
 
-// const LinksStack = createStackNavigator({
-//   Links: LinksScreen,
-// });
-
-// LinksStack.navigationOptions = {
-//   tabBarLabel: 'Messages',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-mail-open' : 'md-link'}
-//     />
-//   ),
-// };
-
 const postStack = createStackNavigator({
   post: PostScreen,
   postConfirm: PostConfirmScreen,
@@ -68,21 +49,7 @@ postStack.navigationOptions = {
       name={Platform.OS === 'ios' ? 'ios-arrow-round-forward' : 'md-options'}
     />
   ),
-}
-
-// const SettingsStack = createStackNavigator({
-//   Settings: SettingsScreen,
-// });
-
-// SettingsStack.navigationOptions = {
-//   tabBarLabel: 'Notification',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon
-//       focused={focused}
-//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-//     />
-//   ),
-// };
+};
 
 const ProfileStack = createStackNavigator({
   Profile: ProfileScreen,
@@ -100,8 +67,6 @@ ProfileStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  // LinksStack,
   postStack,
-  // SettingsStack,
   ProfileStack
 });
