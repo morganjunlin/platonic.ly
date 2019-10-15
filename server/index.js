001723fcf78ed76b88e5edecd4d0039ba7a4b015
morganl92 <morganlinbox@gmail.com>
2019-10-14 20:15:41 -0700

    Removed express.static serving

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT||3000;
-const path = require('path');
+const port = process.env.PORT || 3000;
 const router = require('./router.js');
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', router);
-
-
-//serve the client
-app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
