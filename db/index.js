ea87bfee11d71e314b94094cab0263a530174e52
morganl92 <morganlinbox@gmail.com>
2019-10-14 20:18:11 -0700

    DB auth details modified for localhost env

--- db/index.js
+++ db/index.js
@@ -1,14 +1,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  host: '',//'aws',
-  database: 'justfriends',
-  user: 'ubuntu',
+  host: '',
+  database: 'platonicly',
+  user: 'morganlin',
   password: 'password',
-  // max: 40,
-  // // min: 8,
-  // idleTimeoutMillis: 30000,
-  // connectionTimeoutMillis: 2000,
   port: 5432
 })
 
