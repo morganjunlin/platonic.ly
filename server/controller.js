30cfa456fb241e049e018e32e84ba5ee0bc04dc9
Ubuntu <ubuntu@ip-172-31-25-81.us-east-2.compute.internal>
2019-10-16 20:46:26 +0000

    Fixed controller issue for Edit Profile

--- server/controller.js
+++ server/controller.js
@@ -363,7 +363,7 @@ module.exports = {
   editProfile: (req, res) => {
     const { id } = req.params;
     const { first_name, last_name, description } = req.body;
-    db.query(`UPDATE users SET first_name = '`first_name`', last_name = '`last_name`', description = '`description`' WHERE id = ${id};`)
+    db.query("UPDATE users SET first_name = '" + first_name + "', last_name = '" + last_name + "', description = '" + description + `' WHERE id = ${id};`)
       .then(data =>  res.status(200).send(`User # ${id} has been updated`))
       .catch(e => res.status(404).send(e.stack))
 
